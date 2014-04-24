bcc7437b200ce774799665ab25b830b4342b2cc6
Apolotary <ryskeldiev.b@gmail.com>
2014-04-24 10:54:14 +0900

    first commit

---
+++ MultipeerNTP/BRMultipeerManager.m
@@ -0,0 +1,13 @@
+//
+//  BRMultipeerManager.m
+//  MultipeerNTP
+//
+//  Created by Bektur Ryskeldiev on 4/24/14.
+//  Copyright (c) 2014 Bektur Ryskeldiev. All rights reserved.
+//
+
+#import "BRMultipeerManager.h"
+
+@implementation BRMultipeerManager
+
+@end
